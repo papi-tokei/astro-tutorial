788e6336f5cb932089dae5dd18091a41ad2490fc
papi_tokei <hiroya.jackson@gmail.com>
2023-02-12 16:24:44 -0800

    modify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,9 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 
 // https://astro.build/config
+import vue from "@astrojs/vue";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [],
+  integrations: [vue()]
 });
---
+++ src/components/Sample.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="counter">
+    <p v-if="!isValid">文字数は1以上10以下にしてください</p>
+    <input type="text" v-model="message" />
+    <button @click="submit" :disabled="!isValid">送信</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { ref, computed } from "vue";
+export default {
+  setup() {
+    const message = ref("");
+    const isValid = computed(
+      () => 0 < message.value.length && message.value.length <= 10
+    );
+    const submit = function (_data) {
+      console.log(message.value);
+    };
+    return {
+      isValid,
+      message,
+      submit,
+    };
+  },
+};
+</script>
